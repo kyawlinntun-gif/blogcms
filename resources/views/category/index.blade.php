d037ef1e8a3db6f64dc0240d8debd3deef11a5ec
vagrant <vagrant@homestead>
2021-10-27 14:22:55 +0000

    03 Route - How to use Resource Route, 04 View for Category, and 05 Create New Category View

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,23 @@
+@extends('admin.main')
+
+@section('content')
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Category Control</h1>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">View All Categories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Create New Category</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+@endsection
